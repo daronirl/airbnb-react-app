5ccdbb595a3e8ec44810262d3d87dbf7e8bcb75b
John Prescott <48197513+daronirl@users.noreply.github.com>
2022-05-10 17:37:24 -0700

    Added Image prop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar"
 import Header from "./components/Header"
 import Card from "./components/Card"
-import katieimg from './images/katie-zaferes.png'
+
 
 
 function App() {
@@ -9,9 +9,13 @@ function App() {
     <>
       <Navbar />
       <Header />
-      <Card img="./images/katie-zaferes.png"
-       rating="5.0" reviewCount="6" country="USA" title="Life Lessons with Katie Zaferes"
-       price="136"
+      <Card imgSource={require("./images/katie-zaferes.png")} 
+            imgSourceTwo={require("./images/star.png")}
+            rating="5.0"
+            reviewCount="6" 
+            country="USA"
+            title="Life Lessons with Katie Zaferes"
+            price="136"
        />
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,19 @@
-import katieimg from '../images/katie-zaferes.png'
-import star from '../images/star.png'
+
+
 
 function Card(props) {
   return (
     
         <div className="card--one">
-              <img src={props.katieimg} />
+              <img src={props.imgSource} className="img--katie"/>
           <div className='card-info'>
-              <img src={star} alt="star" className='img--star' />
-              <span> 5.0 </span>
-              <span className='gray'> (6) </span>
-              <span className='gray'> - USA </span>
+              <img src={props.imgSourceTwo} className="img--star"/>
+              {props.rating}
+              <span className='gray'> {props.reviewCount} </span>
+              <span className='gray'> - {props.country} </span>
+              <span className='gray title'> {props.title} </span>
         </div>
-        <p><span className='span--p'>From $136</span> / person</p>
+        <p><span className='span--p'>From {props.price}</span> / person</p>
      </div>
      
   )
